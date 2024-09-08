5bd45b92cf30c5133cd8050cf88fa15f22f5e037
bberghmans <bart.berghmans@spectrumcollege.be>
2024-09-08 14:30:24 +0200

    DataTypes

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿uint A = 0b001001;          // binaire weergave
+Console.WriteLine($"De variabele A = {A}");
+
+uint B = 0xA2;              // hexadecimale weergave
+Console.WriteLine($"De variabele B = {B}");
+
+uint C = A | B;             // bitwise OR
+Console.WriteLine($"De variabele C = {C}");
+
+uint D = ~C;                // bitwise complement
+Console.WriteLine($"De variabele D = {D}");
+
+int E = (int)C;             // type casting
+Console.WriteLine($"De variabele E = {E}");
+
+int F = ~E;                 // bitwise complement (zie regel 10)
+Console.WriteLine($"De variabele F = {F}");
+
+uint G = A >> 2;            // bit shift (wat is de wiskundige operator?)
+Console.WriteLine($"De variabele G = {G}");
+
+uint H = A << 3;            // bit shift (wat is de wiskundige operator?)
+Console.WriteLine($"De variabele H = {H}");
