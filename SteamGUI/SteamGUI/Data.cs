1661a2bbc6d9d69d4436d45e33feb6129b5d88fc
bberghmans <bart.berghmans@spectrumcollege.be>
2024-10-14 15:08:33 +0200

    .

---
+++ SteamGUI/SteamGUI/Data.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamGUI
+{
+    public class Game
+    {
+        public int appid { get; set; }
+        public int playtime_forever { get; set; }
+        public int playtime_windows_forever { get; set; }
+        public int playtime_mac_forever { get; set; }
+        public int playtime_linux_forever { get; set; }
+        public int playtime_deck_forever { get; set; }
+        public int rtime_last_played { get; set; }
+        public int playtime_disconnected { get; set; }
+    }
+
+    public class Response
+    {
+        public int game_count { get; set; }
+        
+        public int GameCount {
+            get {
+                return game_count;
+            }
+        } 
+        
+        public List<Game>? games { get; set; }
+        
+        public ObservableCollection<int>? GameIDs
+        {
+            get {
+                if (games == null) return null;
+                return new ObservableCollection<int>(games.Select(x=>x.appid));
+            }
+        }
+
+    }
+
+    public class RootGameCollection
+    {
+        public Response? response { get; set; }
+        
+        public Response? Response
+        { 
+            get {
+                return response;
+            }
+        }
+    }
+
+
+    public class Appnews
+    {
+        public int appid { get; set; }
+        public List<Newsitem> newsitems { get; set; }
+        public Newsitem FirstItem { get { return newsitems[0]; } }
+        public int count { get; set; }
+    }
+
+    public class Newsitem
+    {
+        public string gid { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public bool is_external_url { get; set; }
+        public string author { get; set; }
+        public string contents { get; set; }
+        public string feedlabel { get; set; }
+        public int date { get; set; }
+        public string feedname { get; set; }
+        public int feed_type { get; set; }
+        public int appid { get; set; }
+        public List<string> tags { get; set; }
+    }
+
+    public class RootNewsForGame: INotifyPropertyChanged
+    {
+        public Appnews appnews { get; set; }
+        public Appnews Appnews
+        {
+            get { return appnews; }
+            set
+            {
+                appnews = value;
+                OnPropertyChanged(nameof(Appnews));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        { 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class MainRoot: INotifyPropertyChanged
+    {
+        public RootNewsForGame rootNews { get; set; }
+        public RootGameCollection rootGameCollection { get; set; }
+
+        public RootNewsForGame RootNews
+        {
+            get { return rootNews; }
+            set
+            {
+                rootNews = value;
+                OnPropertyChanged(nameof(rootNews));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ SteamGUI/SteamGUI/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SteamGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainRoot dataViewModel = new MainRoot();
+        private Dictionary<int, Appnews> appnews = new Dictionary<int, Appnews>();
+
+        const string SteamID = "76561199789007208";
+        const string SteamKey = "8A8B489A2727D8FBADE868AD210E4E23";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadGameCollection(SteamID, SteamKey);
+        }
+
+
+        private async void LoadGameCollection(string steamID, string steamKey)
+        {
+            string URL = $"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={steamKey}&steamid={steamID}&format=json";
+            string result = await DownloadPage(URL);
+            dataViewModel.rootGameCollection = JsonSerializer.Deserialize<RootGameCollection>(result);
+            dataViewModel.rootNews = new RootNewsForGame();
+            this.DataContext = dataViewModel;
+        }
+
+        private async void LoadGameNews(int appID)
+        {
+            /*
+            MainRoot resultdata = (MainRoot)this.DataContext;
+            resultdata.rootGameCollection = dataViewModel.rootGameCollection;
+            resultdata.rootNews = new RootNewsForGame();
+            */
+
+            MainRoot result = (MainRoot)this.DataContext;
+
+            if (appnews.ContainsKey(appID))
+            {
+                result.RootNews.Appnews = appnews[appID];
+            }
+            else
+            {
+                string URL = $"http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid={appID}&count=3&maxlength=300&format=json";
+                string resultstring = await DownloadPage(URL);
+                result.RootNews = JsonSerializer.Deserialize<RootNewsForGame>(resultstring);
+
+                // sanity check
+                if (result.RootNews.appnews.appid == appID)
+                {
+                    appnews.Add(appID, result.RootNews.appnews);
+                }
+            }
+            //this.DataContext = result;
+            //dataViewModel.rootNews = resultdata.rootNews;
+        }
+
+        private async Task<string> DownloadPage(string url)
+        {
+            Debug.Write($"Downloading {url}");
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(new Uri(url)))
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Debug.WriteLine("DONE");
+                    return result;
+                }
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (dataViewModel.rootGameCollection.response == null) return;
+            ListBox listBox = sender as ListBox;
+
+            int selectedValue =  Convert.ToInt32(listBox.SelectedValue);
+
+            LoadGameNews(selectedValue);
+
+        }
+    }
+}
